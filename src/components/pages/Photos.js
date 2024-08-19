de4d801c212b13804fa9ae2f192a73e0dae072c9
Ethan Saso <ethansaso@gmail.com>
2024-08-19 10:06:17 -0700

    Added a couple new images to photography section, and populated JSON with device/date/location

--- src/components/pages/Photos.js
+++ src/components/pages/Photos.js
@@ -5,18 +5,21 @@ import images from '../../images.json';
 const imageContext = require.context('../../assets/img/photography', true);
 
 const MasonryCard = ({ data }) => {
-  const { filename, type, title } = data;
+  const { filename, type, title, date, location, device } = data;
+
+  const deviceNames = {'a52': 'Samsung Galaxy A52', 'tg6': 'Olympus TG-6'}
+
   return (
     <Card key={filename} className='masonry-card' style={{
     }}>
       <div className="masonry-img" style={{ position: 'relative' }}>
         <div className="masonry-caption">
           <div className="masonry-caption-type">DATE</div>
-          <div className="masonry-caption-text">lorem</div>
+          <div className="masonry-caption-text">{date || 'Unknown'}</div>
           <div className="masonry-caption-type">LOCATION</div>
-          <div className="masonry-caption-text">ipsum</div>
+          <div className="masonry-caption-text">{location || 'Unknown'}</div>
           <div className="masonry-caption-type">DEVICE</div>
-          <div className="masonry-caption-text">dolorem</div>
+          <div className="masonry-caption-text">{deviceNames[device] || 'hi'}</div>
         </div>
         <CardImg 
           src={imageContext(`./${filename}`)} 
@@ -29,22 +32,22 @@ const MasonryCard = ({ data }) => {
         <CardTitle tag="h6" style={{ marginBottom: 0, color: '#888' }}>{type}</CardTitle>
         <CardTitle tag="h5" style={{ marginTop: '0', color: '#000' }}>{title}</CardTitle>
       </div>
-      
-      {console.log(data)}
     </Card>
   )
 };
 
 function Photos() {
-  const items = Object.keys(images).map((filename, index) => ({
+  const items = Object.keys(images).map((filename) => ({
     filename,
-    index,
     type: images[filename].type,
-    title: images[filename].title
+    title: images[filename].title,
+    date: images[filename].date,
+    location: images[filename].location,
+    device: images[filename].device,
   }));
 
   return (
-    <Masonry className="masonry" columnGutter={25} overscanBy={Infinity} items={items} render={MasonryCard}/>
+    <Masonry className="masonry" columnGutter={25} columnWidth={300} overscanBy={Infinity} items={items} render={MasonryCard}/>
   );
 }
 
