9d33227d186cef67b5dc7c9b20c92c84a0126b49
Ethan Saso <ethansaso@gmail.com>
2024-08-19 16:25:43 -0700

    added notice that photos and projects are implemented to homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,10 @@
 function Home() {
     return (
-        <div className="App" style={{display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%'}}>
+        <div className="App" style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100%'}}>
             <h2>
                 Site currently under construction (last updated 8/19/24). Please check back soon!
             </h2>
+            <h5>(some projects and my photography are visible, though!)</h5>
         </div>
     );
 }
