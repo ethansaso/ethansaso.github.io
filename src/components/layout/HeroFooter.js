1d685fd1147baa25996d9dc09c3f4fe5c1205f6f
Ethan Saso <ethansaso@gmail.com>
2024-08-25 15:40:26 -0700

    added missing hover effects to hero footer

--- src/components/layout/HeroFooter.js
+++ src/components/layout/HeroFooter.js
@@ -4,13 +4,12 @@ const HeroFooter = () => {
     return (
         <div className="px-2 py-2 w-full border-t border-neutral">
             <nav className="flex items-center justify-between mx-1 lg:mx-4">
-                <a
-                href="https://www.dropbox.com/scl/fi/ki0vn7cingxhckr6bgeql/Resume-mn.pdf?rlkey=6neefmykxwbnxkbo6a45uknou&st=gw1wldfd&dl=1"
-                className="flex items-center border rounded-full p-3 border-bline border-neutral"
-                >
-                    <FontAwesomeIcon size="lg" icon="fa-solid fa-clipboard-list" fixedWidth />
-                    <div className="hidden md:block">Resume</div>
-                </a>
+                <div className="border rounded-full p-3 border-bline border-neutral">
+                    <a href="https://www.dropbox.com/scl/fi/ki0vn7cingxhckr6bgeql/Resume-mn.pdf?rlkey=6neefmykxwbnxkbo6a45uknou&st=gw1wldfd&dl=1" className="hover:text-primary transition-all flex items-center">
+                        <FontAwesomeIcon size="lg" icon="fa-solid fa-clipboard-list" fixedWidth />
+                        <div className="hidden md:block">Resume</div>
+                    </a>
+                </div>
                 <div className="border rounded-full p-3 border-bline border-neutral">
                     <ul className="gap-5 flex">
                         <li className="hover:scale-125 hover:text-sky-600 hover:mx-1 transition-all"><a href="https://www.linkedin.com/in/ethansaso/">
@@ -27,13 +26,14 @@ const HeroFooter = () => {
                         </a></li>
                     </ul>
                 </div>
-                <a
-                href="mailto:ethansaso@berkeley.edu"
-                className="flex items-center border rounded-full p-3 border-bline border-neutral"
+                <div
+                className="border rounded-full p-3 border-bline border-neutral"
                 >
-                    <FontAwesomeIcon size="lg" icon="fa-solid fa-envelope" fixedWidth />
-                    <div className="hidden md:block ml-1">Contact</div>
-                </a>
+                    <a href="mailto:ethansaso@berkeley.edu" className="flex items-center hover:text-primary transition-all">
+                        <FontAwesomeIcon size="lg" icon="fa-solid fa-envelope" fixedWidth />
+                        <div className="hidden md:block ml-1">Contact</div>
+                    </a>
+                </div>
             </nav>
         </div>
     )
