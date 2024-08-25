237749aeff8ad82681ecfa10c1f02c5dc7ad022f
Ethan Saso <ethansaso@gmail.com>
2024-08-25 08:54:08 -0700

    Removed card design from projects

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -12,7 +12,8 @@ const Hero = ({ darkMode }) => {
         </div>
         {darkMode ? (
           <h1 className="text-xl">
-            I'm a <span className="text-secondary font-semibold">full-stack developer </span>
+            I'm a <span className="text-secondary font-semibold">software engineer </span>
+            and <span className="text-secondary font-semibold">full-stack developer </span>
             from Berkeley, CA dedicated to the intersection of
             <span className="text-accent font-semibold"> digital design</span> and{" "}
             <span className="text-accent font-semibold">environmental systems</span>.
--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -7,33 +7,33 @@ const Projects = () => {
         <div id="projects" className="container flex flex-col max-w-7xl mx-auto items-center justify-center mt-16 px-12 lg:px-16">
             <h1 className="text-primary text-5xl text-left mb-8 lg:mb-12 w-full border-b border-neutral font-semibold">Projects</h1>
             <div className="project-section">
-                <div className="card border border-neutral bg-base-200 flex flex-col xl:flex-row justify-between mb-12">
-                    <img className="h-auto rounded-t-2xl lg:rounded-tr-none lg:rounded-l-2xl border-b border-neutral xl:h-96 w-auto object-contain" src={TaxoKeys} alt="TaxoKeys"/>
-                    <div className="w-full p-4 flex flex-col">
+                <div className="flex flex-col xl:flex-row justify-between mb-8 items-center">
+                    <img className="h-auto rounded-2xl object-cover xl:h-96 w-[40%]" src={TaxoKeys} alt="TaxoKeys"/>
+                    <div className="flex-grow p-12 flex flex-col xl:w-[60%]">
                         <div className="w-full border-b border-neutral mb-1">
                             <h1 className="text-4xl font-semibold text-secondary">TaxoKeys</h1>
                             <h2 className="text-lg text-warning">MERN stack, Python, Digital Ocean</h2>
                         </div>
                         <p className="text-lg">Using dynamic algorithms, TaxoKeys generates elegant, flowchart-style guides to identification in real time,
                         resolving complex relationships between species with minimal error.</p>
-                        <a href="https://taxokeys.org" className="w-full text-center rounded-2xl border border-neutral mt-5 xl:mt-auto p-3 hover:border-base-content bg-base-100 transition-all">Visit Site</a>
+                        <a href="https://taxokeys.org" className="w-full text-center rounded-2xl border border-neutral mt-5 p-3 hover:border-base-content bg-base-100 transition-all">Visit Site</a>
                     </div>
                 </div>
-                <div className="card border border-neutral bg-base-200 flex flex-col xl:flex-row-reverse justify-between mb-12">
-                    <img className="h-auto rounded-t-2xl lg:rounded-tl-none lg:rounded-r-2xl border-b border-neutral xl:h-96 w-auto object-contain" src={WebRecon2} alt="Web Recon 2"/>
-                    <div className="w-full p-4 flex flex-col">
+                <div className="flex flex-col xl:flex-row-reverse justify-between mb-8 items-center">
+                    <img className="h-auto rounded-2xl object-cover xl:h-96 w-[40%]" src={WebRecon2} alt="Web Recon 2"/>
+                    <div className="flex-grow p-12 flex flex-col xl:w-[60%]">
                         <div className="w-full border-b border-neutral mb-1">
                             <h1 className="text-4xl font-semibold text-secondary">WebRecon2</h1>
                             <h2 className="text-lg text-warning">Golang, Amass, DNSGen, ...</h2>
                         </div>
                         <p className="text-lg">WebRecon2 leverages a variety of best-in-class cybersecurity tools
                             to automate a workflow for performing subdomain enumeration.</p>
-                        <a href="https://github.com/sammooredev/WebRecon2" className="w-full text-center rounded-2xl border border-neutral mt-5 xl:mt-auto p-3 hover:border-base-content bg-base-100 transition-all">Visit Site</a>
+                        <a href="https://github.com/sammooredev/WebRecon2" className="w-full text-center rounded-2xl border border-neutral mt-5 p-3 hover:border-base-content bg-base-100 transition-all">Visit Site</a>
                     </div>
                 </div>
-                <div className="card border border-neutral bg-base-200 flex flex-col xl:flex-row justify-between mb-12">
-                    <img className="h-auto rounded-t-2xl lg:rounded-tr-none lg:rounded-l-2xl border-b border-neutral xl:h-96 w-auto object-contain" src={Volleyball} alt="Cal Men's Volleyball"/>
-                    <div className="w-full p-4 flex flex-col">
+                <div className="flex flex-col xl:flex-row justify-between mb-8 items-center">
+                    <img className="h-auto rounded-2xl object-cover xl:h-96 w-[40%]" src={Volleyball} alt="Cal Men's Volleyball"/>
+                    <div className="flex-grow p-12 flex flex-col xl:w-[60%]">
                         <div className="w-full border-b border-neutral mb-1">
                             <div>
                                 <h1 className="text-4xl font-semibold text-secondary">Cal Men's Volleyball <span className="text-error">(WIP)</span></h1>
@@ -41,7 +41,7 @@ const Projects = () => {
                             <h2 className="text-lg text-warning">React.js, Github Pages</h2>
                         </div>
                         <p className="text-lg">Official website for UC Berkeley's Division-I Men's Volleyball Team.</p>
-                        <a href="https://ethansaso.github.io/cal-mens-volleyball" className="w-full text-center rounded-2xl border border-neutral mt-5 xl:mt-auto p-3 hover:border-base-content bg-base-100 transition-all">Visit Site</a>
+                        <a href="https://ethansaso.github.io/cal-mens-volleyball" className="w-full text-center rounded-2xl border border-neutral mt-5 p-3 hover:border-base-content bg-base-100 transition-all">Visit Site</a>
                     </div>
                 </div>
             </div>
