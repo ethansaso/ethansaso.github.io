afb407339551d08f214e0c1b4c521dcc323b5597
Ethan Saso <ethansaso@gmail.com>
2024-09-05 10:27:12 -0700

    Moved volleyball project up higher to reflect work invested

--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -30,17 +30,16 @@ const Projects = () => {
                 <Reveal width='md:w-[45%] lg:w-full flex-grow' from='right'>
                     <div className="glass-low rounded-2xl flex flex-col lg:flex-row-reverse items-center flex-grow lg:p-8 h-full lg:h-[375px]">
                         <div className="lg:h-full w-full lg:w-[40%] flex items-center">
-                            <img className="w-full aspect-[4/3] h-full object-cover rounded-t-2xl lg:rounded-2xl border border-neutral" src={WebRecon2} alt="Web Recon 2"/>
+                            <img className="w-full aspect-[4/3] h-full object-cover rounded-t-2xl md:rounded-2xl border border-neutral" src={Volleyball} alt="Cal Men's Volleyball"/>
                         </div>
                         <div className="h-full p-8 lg:p-0 flex-grow lg:pr-8 flex flex-col lg:w-[60%] w-full">
                             <div className="w-full border-b border-neutral mb-2">
-                                <h1 className="text-4xl font-semibold text-secondary">WebRecon2</h1>
-                                <h2 className="text-md text-accent">Golang, Amass, DNSGen, ...</h2>
+                                <h1 className="text-4xl font-semibold text-secondary">Cal Men's Volleyball <span className="text-error">(WIP)</span></h1>
+                                <h2 className="text-md text-accent">React.js, Github Pages</h2>
                             </div>
-                            <p className="text-lg flex-grow">WebRecon2 leverages a variety of best-in-class cybersecurity tools
-                                to automate a workflow for performing subdomain enumeration.</p>
+                            <p className="text-lg flex-grow">Official website for UC Berkeley's Division-I Men's Club Volleyball Team.</p>
                             <div class="layered-button-frame">
-                                <a href="https://github.com/sammooredev/WebRecon2" className="layered-button-a">
+                                <a href="https://ethansaso.github.io/cal-mens-volleyball" className="layered-button-a">
                                     <div class="layered-button">Visit Site</div>
                                 </a>
                             </div>
@@ -50,16 +49,17 @@ const Projects = () => {
                 <Reveal width='w-full' from='left'>
                     <div className="glass-low rounded-2xl flex flex-col md:flex-row items-center flex-grow md:p-8 md:h-[375px]">
                         <div className="md:h-full w-full md:w-[40%] flex items-center"> 
-                            <img className="w-full aspect-[4/3] h-full object-cover rounded-t-2xl md:rounded-2xl border border-neutral" src={Volleyball} alt="Cal Men's Volleyball"/>
+                            <img className="w-full aspect-[4/3] h-full object-cover rounded-t-2xl lg:rounded-2xl border border-neutral" src={WebRecon2} alt="Web Recon 2"/>
                         </div>
                         <div className="h-full p-8 md:p-0 flex-grow md:pl-8 flex flex-col md:w-[60%] w-full">
                             <div className="w-full border-b border-neutral mb-2">
-                                    <h1 className="text-4xl font-semibold text-secondary">Cal Men's Volleyball <span className="text-error">(WIP)</span></h1>
-                                    <h2 className="text-md text-accent">React.js, Github Pages</h2>
+                                <h1 className="text-4xl font-semibold text-secondary">WebRecon2</h1>
+                                <h2 className="text-md text-accent">Golang, Amass, DNSGen, ...</h2>
                             </div>
-                            <p className="text-lg flex-grow">Official website for UC Berkeley's Division-I Men's Club Volleyball Team.</p>
+                            <p className="text-lg flex-grow">WebRecon2 leverages a variety of best-in-class cybersecurity tools
+                                to automate a workflow for performing subdomain enumeration.</p>
                             <div class="layered-button-frame">
-                                <a href="https://ethansaso.github.io/cal-mens-volleyball" className="layered-button-a">
+                                <a href="https://github.com/sammooredev/WebRecon2" className="layered-button-a">
                                     <div class="layered-button">Visit Site</div>
                                 </a>
                             </div>
